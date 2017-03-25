552de75ddc6eefb10e69bbeccf1d27bfac05581c
Andres Rodriguez <ar15@bu.edu>
2017-03-25 17:52:49 -0400

    Duy is God.

--- happy_tweet/db.py
+++ happy_tweet/db.py
@@ -1,16 +1,20 @@
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client['test_db']
-
-
-def insert(value, collection):
-    col = db[collection]
-    insert_id = col.update({'_id': value['_id']}, value, upsert=True)
-    return insert_id
-
-
-def search(key, collection):
-    col = db[collection]
-    data = col.find_one(key)
-    return data
+# client = MongoClient()
+# db = client['test_db']
+#
+#
+# def insert(value, collection):
+#     col = db[collection]
+#     insert_id = col.update({'_id': value['_id']}, value, upsert=True)
+#     return insert_id
+#
+#
+# def search(key, collection):
+#     col = db[collection]
+#     data = col.find_one(key)
+#     return data
+def insert(a,b):
+    pass
+def search(a,b):
+    pass
--- happy_tweet/emotionCalculations.py
+++ happy_tweet/emotionCalculations.py
@@ -14,15 +14,46 @@ def emo_tweet(tweet):
     print("Here" + tweet)
     return 0
 
+def average_dict(d):
+    r = {}
+    for x in d:
+        for k, v in x.items():
+            try:
+                if r[k]:
+                    r[k].append(v)
+            except KeyError:
+                r[k] = [v]
+    rr = {}
+    for k, v in r.items():
+        rr[k] = sum(v)/len(v)*1.0
+    return rr
 
+
+def avg_emotion(user):
+    ts = user.tweets
+    if ts:
+        tweets =ts
+        tweet_emotions = []
+        for tweet in tweets:
+            tweet_emotions.append(tweet['emotions'])
+        return tweet_emotions
+"""
 def individual_happiness(tweets):
     for tweet in tweets:
         text = tweet['text']
         emotion = emo_tweet(text)
         tweet['emotion'] = emotion
     return tweets
+"""
 
 
 if __name__ == '__main__':
-    u = User('@gaspardetienne9')
-    print(individual_happiness(u.get_tweets()))
+    # u = User('@gaspardetienne9')
+    # u.get_tweets()
+    # print(avg_emotion(u))
+    print(average_dict(
+        [{"a": 2, "b": 3},
+        {"a": 5, "b": 3},
+        {"a": 7, "b": 3},
+    ]))
+
