b6fc2cb7f85105b296aa94c6ea0bbb2731be68a1
adeshiel <ashley.deshields.2016@gmail.com>
2017-03-25 11:51:22 -0400

    Returns the most probable emotion

--- emotionCalculations.py
+++ emotionCalculations.py
@@ -4,18 +4,20 @@ indicoio.config.api_key = 'b2ff22240dc7532835daca4712d48bfb'
 
 
 class EmotionCalc:
-    def __init__(self, age, tweet):
+    def __init__(self, age, tweet, location):
         self.tweet = tweet
         self.age = age
+        self.location = location
 
     def __repr__(self):
         s = 'Name:' + self.name + '\n'
         s += 'Tweet:' + self.tweet + '\n'
+        s += 'Location' + self.location + '\n'
 
     def mostlikelyemotion(self):
         # single example
-        print(indicoio.emotion("I did it. I got into Grad School. Not just any program, but a GREAT program. :-)"))
-        test = indicoio.emotion("I did it. I got into Grad School. Not just any program, but a GREAT program. :-)")
+        print(indicoio.emotion(self.tweet))
+        test = indicoio.emotion(self.tweet)
 
         # batch example
         # emolist = indicoio.emotion([
@@ -23,3 +25,4 @@ class EmotionCalc:
         #    "Like seriously my life is bleak, I have been unemployed for almost a year."
         # ])
         print(max(test, key=test.get))
+        
