57d5358e5ee6d8b906f24371057d098f790e65fa
Andres Rodriguez <ar15@bu.edu>
2017-03-25 21:14:25 -0400

    Why you no work?!?

--- happy_tweet/emotionTest.py
+++ happy_tweet/emotionTest.py
@@ -1,16 +1,17 @@
 from happy_tweet import User
+from happy_tweet import get_user
 
 if __name__ == '__main__':
-    username = input("What's your name, user?")
-    subject = User(username)
-    list_of_others = {}
+    other_emotions = {}
     username_list = ["JMills21478", "Lynds_eey", "Duuuval", "Norphsidee", "beingmesince96", "april_aries17", "Moliblog",
                      "_stayygorgeous", "JulioCN93", "Hipcheck1967", "SirAWilliams80", "His_SmokingGun",
                      "elirosstheboss",
                      "ThiahArmani_", "MDCPS", "DannyEspinalHD", "ThiahArmani_", "ijump14", "_Whiitneey", "dominictw",
                      "yesimbby", ]
     for x in username_list:
-        a = User(x)
-        list_of_others[x] = x["emotions"]
+        a = get_user(x)
+        other_emotions[x] = x['emotions']
+    print(other_emotions)
+
 
 
