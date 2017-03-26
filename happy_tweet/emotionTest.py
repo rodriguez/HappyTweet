e7941fb16a076ada70d1c907ca7de6da786f1707
Andres Rodriguez <ar15@bu.edu>
2017-03-25 21:56:40 -0400

    Added functionality to emotion calculating.

--- happy_tweet/emotionTest.py
+++ happy_tweet/emotionTest.py
@@ -1,4 +1,12 @@
 from happy_tweet import get_user
+from happy_tweet.emo_calc import merge_dict, average_dict
+
+
+def max_average_emo(username):
+    user = get_user(username)
+    tweets = user['tweets']
+    emotions = [tweet['emotions'] for tweet in tweets]
+    return average_dict(merge_dict(emotions))
 
 if __name__ == '__main__':
     other_emotions = {}
@@ -7,12 +15,21 @@ if __name__ == '__main__':
                      "elirosstheboss",
                      "ThiahArmani_", "MDCPS", "DannyEspinalHD", "ThiahArmani_", "ijump14", "_Whiitneey", "dominictw",
                      "yesimbby", ]
-    # for username in username_list:
-    #     user = get_user(username)
-    #     tweets = user['tweets']
-    #     emotions = [tweet['emotions'] for tweet in tweets]
-    #     print(emotions)
-    user = get_user(username_list[0])
-    tweets = user['tweets']
-    emotions = [tweet['emotions'] for tweet in tweets]
-    print(emotions)
+    r = []
+    for username in username_list:
+        try:
+            emo = max_average_emo(username)
+            r.append({
+                'username':username,
+                'emo':emo,
+            })
+        except:
+            print("Error")
+            continue
+    print(r)
+    # user = get_user(username_list[0])
+    # tweets = user['tweets']
+    # emotions = [tweet['emotions'] for tweet in tweets]
+    # print(emotions)
+
+
